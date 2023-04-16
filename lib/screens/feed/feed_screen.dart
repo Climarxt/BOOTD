b9837d2ade9fb9575c18a531c6cc2badcd7c487b
Climarxt <contact@christianbastide.com>
2023-04-16 23:45:28 +0200

    CleanCode - FeedScreen

--- lib/screens/feed/feed_screen.dart
+++ lib/screens/feed/feed_screen.dart
@@ -68,61 +68,65 @@ class _FeedScreenState extends State<FeedScreen> with TickerProviderStateMixin {
       },
       builder: (context, state) {
         return Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 130,
-            centerTitle: true,
-            backgroundColor: white,
-            iconTheme: const IconThemeData(
-              color: black,
-            ),
-            elevation: 0,
-            title: Column(
-              children: [
-                TextField(
-                  controller: _textController,
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey[200],
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: const BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
-                    ),
-                    contentPadding: EdgeInsets.all(5),
-                    prefixIcon: IconButton(
-                      icon: const Icon(Icons.search),
-                      onPressed: () {
-                        context.read<SearchCubit>().clearSearch();
-                        _textController.clear();
-                      },
-                    ),
-                  ),
-                  textInputAction: TextInputAction.search,
-                  textAlignVertical: TextAlignVertical.center,
-                  onSubmitted: (value) {
-                    if (value.trim().isNotEmpty) {
-                      context.read<SearchCubit>().searchUsers(value.trim());
-                    }
-                  },
-                ),
-                SizedBox(height: 12),
-                TabBar(
-                    indicatorSize: TabBarIndicatorSize.tab,
-                    indicator: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: couleurBleuClair2),
-                    controller: tabController,
-                    labelColor: white,
-                    unselectedLabelColor: Colors.white.withOpacity(0.20),
-                    tabs: tabs),
-              ],
-            ),
-          ),
+          appBar: _buildAppBar(tabController),
           body: _buildBody(state),
         );
       },
+    );
+  }
+
+  AppBar _buildAppBar(TabController tabController) {
+    return AppBar(
+      toolbarHeight: 130,
+      centerTitle: true,
+      backgroundColor: white,
+      iconTheme: const IconThemeData(
+        color: black,
+      ),
+      elevation: 0,
+      title: Column(
+        children: [
+          TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              fillColor: Colors.grey[200],
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+                borderSide: const BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
+                ),
+              ),
+              contentPadding: EdgeInsets.all(5),
+              prefixIcon: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  context.read<SearchCubit>().clearSearch();
+                  _textController.clear();
+                },
+              ),
+            ),
+            textInputAction: TextInputAction.search,
+            textAlignVertical: TextAlignVertical.center,
+            onSubmitted: (value) {
+              if (value.trim().isNotEmpty) {
+                context.read<SearchCubit>().searchUsers(value.trim());
+              }
+            },
+          ),
+          SizedBox(height: 12),
+          TabBar(
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: couleurBleuClair2),
+              controller: tabController,
+              labelColor: white,
+              unselectedLabelColor: Colors.white.withOpacity(0.20),
+              tabs: tabs),
+        ],
+      ),
     );
   }
 
