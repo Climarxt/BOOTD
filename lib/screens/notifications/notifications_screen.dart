c89cffd68eedf9b9a058de9c762b1710a6e5464c
Climarxt <contact@christianbastide.com>
2023-04-16 09:45:07 +0200

    Fix - null - cleanCode

--- lib/screens/notifications/notifications_screen.dart
+++ lib/screens/notifications/notifications_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../config/config.dart';
+import '../../models/models.dart';
 import '/screens/notifications/bloc/notifications_bloc.dart';
 import '/screens/notifications/widgets/widgets.dart';
 import '/widgets/widgets.dart';
 
+// This class represents the Notifications screen in the application.
 class NotificationsScreen extends StatelessWidget {
   static const String routeName = '/notifications';
 
@@ -14,48 +15,63 @@ class NotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: mobileBackgroundColor,
-        iconTheme: const IconThemeData(
-          color: black,
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.message,
-              size: 30,
-            ),
-            onPressed: () {},
-          ),
-          const SizedBox(
-            width: 13,
-          ),
-        ],
-        elevation: 3,
-        title: const Text(
-          'Notifications',
-          style: TextStyle(color: black),
-        ),
-      ),
-      body: BlocBuilder<NotificationsBloc, NotificationsState>(
-        builder: (context, state) {
-          switch (state.status) {
-            case NotificationsStatus.error:
-              return CenteredText(text: state.failure.message);
-            case NotificationsStatus.loaded:
-              return ListView.builder(
-                itemCount: state.notifications.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final notification = state.notifications[index];
-                  return NotificationTile(notification: notification!);
-                },
-              );
-            default:
-              return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+      appBar: _buildAppBar(),
+      body: _buildNotificationsList(),
     );
+  }
+
+  // Builds and returns the AppBar for the Notifications screen.
+  AppBar _buildAppBar() {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: mobileBackgroundColor,
+      iconTheme: const IconThemeData(color: black),
+      actions: const <Widget>[
+        Icon(Icons.message, size: 30),
+        SizedBox(width: 13),
+      ],
+      elevation: 3,
+      title: const Text('Notifications', style: TextStyle(color: black)),
+    );
+  }
+
+  // Builds and returns the notifications list by handling different states.
+  Widget _buildNotificationsList() {
+    return BlocBuilder<NotificationsBloc, NotificationsState>(
+      builder: (context, state) {
+        switch (state.status) {
+          case NotificationsStatus.error:
+            return _buildErrorMessage(state.failure.message);
+          case NotificationsStatus.loaded:
+            return _buildLoadedNotificationsList(state.notifications);
+          default:
+            return _buildLoadingIndicator();
+        }
+      },
+    );
+  }
+
+  // Builds and returns a widget displaying an error message.
+  Widget _buildErrorMessage(String message) {
+    return CenteredText(text: message);
+  }
+
+  // Builds and returns the list of notifications when the state is loaded.
+  Widget _buildLoadedNotificationsList(List<Notif?> notifications) {
+    return ListView.builder(
+      itemCount: notifications.length,
+      itemBuilder: (BuildContext context, int index) {
+        final notification = notifications[index];
+        if (notification == null) {
+          return Container();
+        }
+        return NotificationTile(notification: notification);
+      },
+    );
+  }
+
+  // Builds and returns a loading indicator when the state is not yet loaded.
+  Widget _buildLoadingIndicator() {
+    return const Center(child: CircularProgressIndicator());
   }
 }
